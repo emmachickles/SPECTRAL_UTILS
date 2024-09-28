4a40890600729243a93e6439bd013ee02c050b35
emmachickles <echickle@wellesley.edu>
2024-09-28 15:42:01 -0400

    before changing normalize_continuum to polyfit

--- SPECTRAL_UTILS/model_utils.py
+++ SPECTRAL_UTILS/model_utils.py
@@ -72,20 +72,23 @@ def interpolate(wave, modwave, modgrid, Teff_grid, lgg_grid, temp, logg):
 
     return flux
 
-def broadening(Teff, logg, vsini, obswave, modwave, modgrid, Teff_grid, lgg_grid,
-               R=7000, epsilon=0.5):
+def interpolate_linear(obswave, modwave, modgrid, Teff_grid, lgg_grid, temp, logg):
+    '''Interpolate model atmosphere to an evenly spaced wavelength array.'''
+
+    wmin = np.min(obswave) - 10
+    wmax = np.max(obswave) + 10
+    nbin = np.count_nonzero( (modwave > wmin) * (modwave < wmax) ) 
+    linwave = np.linspace(wmin, wmax, 10*nbin)
+    modflux = interpolate(linwave, modwave, modgrid, Teff_grid, lgg_grid,
+                          temp, logg)
+
+    return linwave, modflux
+    
+def broadening(obswave, linwave, modwave, modflux, vsini, R=7000, epsilon=0.5):
 
     from PyAstronomy.pyasl import instrBroadGaussFast, fastRotBroad
     from scipy.ndimage import map_coordinates    
     
-    # Interpolate model grid to an evenly spaced wavelength array
-    wmin = np.min(obswave)*0.95
-    wmax = np.max(obswave)*1.05
-    nbin = np.count_nonzero( (modwave > wmin) * (modwave < wmax) ) 
-    linwave = np.linspace(wmin, wmax, 10*nbin)
-    modflux = interpolate(linwave, modwave, modgrid, Teff_grid, lgg_grid,
-                          Teff, logg)
-
     # Apply instrumental broadening
     modflux = instrBroadGaussFast(linwave, modflux, R, edgeHandling='firstlast')
     
@@ -93,10 +96,8 @@ def broadening(Teff, logg, vsini, obswave, modwave, modgrid, Teff_grid, lgg_grid
     modflux = fastRotBroad(linwave, modflux, epsilon, vsini)
 
     # Trim edge effects
-    wmin = np.min(obswave)
-    wmax = np.max(obswave)
-    inds = np.nonzero( (linwave > wmin) * (linwave < wmax) )
-    linwave, modflux = linwave[inds], modflux[inds]
+    inds = np.nonzero( (linwave > np.min(obswave)) * (linwave < np.max(obswave)) )
+    linwave = linwave[inds]
     
     # Interpolate model grid to observed wavelength grid
     windex = np.interp(obswave, linwave, np.arange(len(linwave)))
@@ -139,62 +140,7 @@ def convert_to_physical(wave, flux, r, d):
 
     return flux
 
-def fit_lines(obswave, obsflux, obsferr, modwave, modgrid, Teff_grid, lgg_grid,
-              R=7000, epsilon=0.5):
-    from scipy.optimize import minimize
-    from SPECTRAL_UTILS.spec_utils import normalize_continuum
-
-    def mse(params, obswave, obsflux, obsferr, modwave, modgrid, Teff_grid,
-            lgg_grid, R, epsilon):
-        
-        Teff, logg, vsini = params
-        mse = 0
-        
-        for i in range(len(obsflux)):
-            
-            # Interpolate model spectrum and apply braodening effects
-            modflux = broadening(Teff, logg, vsini, obswave[i], modwave, modgrid,
-                                 Teff_grid, lgg_grid, R, epsilon)
-
-            # Normalize
-            modflux = normalize_continuum(obswave[i], modflux)
-
-            # Calculated weighted squared error
-            # w = 1 / (obsferr[i]**2)
-            # mse += np.sum( w * (modflux - obsflux[i])**2 ) / np.sum( w )
-            mse += np.sum( (modflux - obsflux[i])**2 )
-
-        return mse
-    
-    p0 = [9000, 6, 500]
-    bounds = [[5000, 50000], [5, 7], [1, 1500]]
-
-    args = (obswave, obsflux, obsferr, modwave, modgrid, Teff_grid, lgg_grid,
-            R, epsilon)
-
-    # p0 = [9000, 6, 1]
-    # print(p0)
-    # print(mse(p0, obswave, obsflux, obsferr, modwave, modgrid, Teff_grid,
-    #         lgg_grid, R, epsilon))
-    # p1 = [50000, 5, 1]
-    # print(p1)
-    # print(mse(p1, obswave, obsflux, obsferr, modwave, modgrid, Teff_grid,
-    #         lgg_grid, R, epsilon))    
-    # import pdb
-    # pdb.set_trace()
-    
-    res = minimize(mse, p0, bounds=bounds, args=args, method='Nelder-Mead')
-
-    Teff, logg, vsini = res.x
-    print('Effective temperature: '+str(Teff)+' K')
-    print('Logg: '+str(logg))
-    print('vsini: '+str(vsini)+' km/s')
-          
-
-    return res
-    
-def plot_line_fits(Teff, logg, vsini, modwave, modgrid, Teff_grid, lgg_grid,
-                   wave_line, flux_line, wave0, out_dir, R=7000, epsilon=0.5,
+def plot_line_fits(wave, obsflux, modflux, wave0, out_dir,
                    pad=0.3):
 
     import matplotlib.pyplot as plt
@@ -204,25 +150,20 @@ def plot_line_fits(Teff, logg, vsini, modwave, modgrid, Teff_grid, lgg_grid,
     plt.figure(figsize=(4,4))
     plt.ylabel('Relative Flux')
     plt.xlabel(r'$\Delta\lambda(Å)$')
-    ymin, ymax = 0., 1.02+len(wave_line)*pad
+    ymin, ymax = 0., 1.02+len(wave)*pad
     plt.yticks(np.arange(ymin, ymax, pad))
     plt.ylim([0., plt.ylim()[1]])
 
     for i, w0 in enumerate(wave0):
-        plt.plot(wave_line[i]-w0, flux_line[i]+pad*i, '-k', lw=0.5)
+        f = normalize_continuum(wave[i], obsflux[i])
+        plt.plot(wave[i]-w0, f+pad*i, '-k', lw=0.5)
+        f = normalize_continuum(wave[i], modflux[i])
+        plt.plot(wave[i]-w0, f+pad*i, '-r', lw=0.5)        
 
-        # Interpolate and normalize model spectrum
-        linwave = np.linspace(np.min(wave_line[i]), np.max(wave_line[i]), 1000)
-        modflux = broadening(Teff, logg, vsini, linwave, modwave, modgrid,
-                             Teff_grid, lgg_grid, R, epsilon)
-        modflux = normalize_continuum(linwave, modflux)
-
-        plt.plot(linwave-w0, modflux+pad*i, '-r', lw=0.5)        
-
-    plt.text(0, 0.4, 'Teff = {} K'.format(int(np.round(Teff, -1))), ha='center')
-    plt.text(0, 0.3, 'logg={}'.format(np.round(logg, 2)), ha='center')
-    plt.text(0, 0.2, 'vsini={} km/s'.format(int(np.round(vsini, -1))),
-             ha='center')    
+    # plt.text(0, 0.4, 'Teff = {} K'.format(int(np.round(Teff, -1))), ha='center')
+    # plt.text(0, 0.3, 'logg={}'.format(np.round(logg, 2)), ha='center')
+    # plt.text(0, 0.2, 'vsini={} km/s'.format(int(np.round(vsini, -1))),
+    #          ha='center')    
     plt.tight_layout()
     
     fname = out_dir+'line_fits.png'
